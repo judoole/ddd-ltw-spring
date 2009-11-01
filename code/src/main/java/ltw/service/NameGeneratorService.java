9a09e1970c5a1983e02f820b3618aaa957ef150e
Ole Christian Langfjæran <judoole@gmail.com>
2009-11-01 23:11:19 +0100

    Some more files

---
+++ code/src/main/java/ltw/service/NameGeneratorService.java
@@ -0,0 +1,5 @@
+package ltw.service;
+
+public interface NameGeneratorService {
+    String generateName();
+}
---
+++ code/src/main/java/ltw/service/PirateNameGenerator.java
@@ -0,0 +1,20 @@
+package ltw.service;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PirateNameGenerator implements NameGeneratorService{
+
+    public String generateName() {
+        String[] names = new String[]{"Cap'n Blaine Greenbeard", "Wally Knocknees", "Stutterin' Ross Smithe"};
+        return names[new Random().nextInt(2)];
+    }
+    
+    public static void main(String[] args) {
+        String generateName = new PirateNameGenerator().generateName();
+        System.out.println(generateName);
+    }
+
+}
